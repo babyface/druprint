2086ff2deb5b503ea90aa89c26e3623e762286eb
Christopher Joslyn <christopher.joslyn@gmail.com>
2009-09-21 13:11:18 -0400

    Added comment regarding backup migrate configuration.

--- set_backup_migrate.sh
+++ set_backup_migrate.sh
@@ -5,3 +5,6 @@ drush eval "variable_set('backup_migrate_schedule_backup_keep','72');"
 drush eval "variable_set('backup_migrate_timestamp_format','Y-m-d_H-i-s');"
 drush eval "variable_set('backup_migrate_compression','gzip');"
 drush eval "variable_set('backup_migrate_destination','save');"
+
+# You still have to visit the backup_migrate page to create the needed directories.
+# Haven't found a better way yet.
